172cbe75e3f9376e1ae6aec3a6f4c771cf68a260
Adrián González Galván <alu0101321219@ull.edu.es>
2022-05-14 17:44:29 +0000

    Comentamos el alias de tipo género

--- src/genre/genre.ts
+++ src/genre/genre.ts
@@ -1,2 +1,6 @@
+/**
+ * Alias de tipo empleado para catalogar los distintos géneros musicales
+ * que englobamos en nuestra base de datos.
+ */
 export type Genre = 'Rock' | 'Heavy Metal' | 'Reggaeton' | 'Jazz' | 'Pop' | 'Rap' |
   'Hip-hop' | 'Trap' | 'Urban' | 'Latino' | 'Bachata' | 'Music alternative' | 'Electro';
