935ff3cc4095352c3940583471e39f0dec27636f
Adrián González Galván <alu0101321219@ull.edu.es>
2022-05-18 14:34:15 +0000

    Modificacion del envio por defecto

--- src/routers/default.ts
+++ src/routers/default.ts
@@ -3,5 +3,7 @@ import * as express from 'express';
 export const defaultRouter = express.Router();
 
 defaultRouter.all('*', (_, res) => {
-  res.status(501).send();
+  res.status(501).send({
+    error: 'This route is not implemented',
+  });
 });
