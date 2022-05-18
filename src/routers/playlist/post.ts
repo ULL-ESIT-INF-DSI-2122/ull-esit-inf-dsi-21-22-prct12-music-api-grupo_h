5b2195bf7a90d20230d606e257d62a889dc6b024
Adrián González Galván <alu0101321219@ull.edu.es>
2022-05-18 15:49:43 +0000

    Cambios en el método post de playlist

--- src/routers/playlist/post.ts
+++ src/routers/playlist/post.ts
@@ -4,15 +4,11 @@ import {Playlist} from '../../models/playlist';
 export const postPlaylistRouter = express.Router();
 
 postPlaylistRouter.post('/playlist', (req, res) => {
-  const playlist = new Playlist(req.query);
+  const playlist = new Playlist(req.body);
 
-  if (req.query.genre) {
-    playlist.save().then((playlist) => {
-      res.status(201).send(playlist);
-    }).catch((error) => {
-      res.status(400).send(error);
-    });
-  } else {
-    res.status(400).send({success: false, message: 'Se debe introducir el género de la canción'});
-  }
+  playlist.save().then((playlist) => {
+    res.status(201).send(playlist);
+  }).catch((err) => {
+    res.status(400).send(err);
+  });
 });
